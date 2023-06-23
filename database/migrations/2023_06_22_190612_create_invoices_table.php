ebb8c7c6f3cdbf25893b0b16fb044b13cfce3da3
monasrour <mona.omar.srour@gmail.com>
2023-06-23 06:31:57 +0300

    make sections table and its controller

--- database/migrations/2023_06_22_190612_create_invoices_table.php
+++ database/migrations/2023_06_22_190612_create_invoices_table.php
@@ -12,7 +12,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('invoices', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('invoice_number');
+            $table->date('invoice_Date');
+            $table->date('due_date');
+            $table->string('product');
+            $table->string('section');
+            $table->string('discount');
+            $table->string('rate_vat');
+            $table->decimal('value_vat',8,2);
+            $table->decimal('Total',8,2);
+            $table->string('Status', 50);
+            $table->integer('value_status');
+            $table->text('note')->nullable();
+            $table->string('user');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- resources/views/invoices/invoices.blade.php
+++ resources/views/invoices/invoices.blade.php
@@ -1,46 +1,95 @@
 @extends('layouts.master')
+@section('title')
+	قائمة الفواتير
+@endsection
 @section('css')
+<!-- Internal Data table css -->
+<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
 @endsection
 @section('page-header')
 				<!-- breadcrumb -->
 				<div class="breadcrumb-header justify-content-between">
 					<div class="my-auto">
 						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">Pages</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ Empty</span>
+							<h4 class="content-title mb-0 my-auto">الفواتير</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ قائمة الفواتير</span>
 						</div>
 					</div>
-					<div class="d-flex my-xl-auto right-content">
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-info btn-icon ml-2"><i class="mdi mdi-filter-variant"></i></button>
-						</div>
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-danger btn-icon ml-2"><i class="mdi mdi-star"></i></button>
-						</div>
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-warning  btn-icon ml-2"><i class="mdi mdi-refresh"></i></button>
-						</div>
-						<div class="mb-3 mb-xl-0">
-							<div class="btn-group dropdown">
-								<button type="button" class="btn btn-primary">14 Aug 2019</button>
-								<button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" id="dropdownMenuDate" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<span class="sr-only">Toggle Dropdown</span>
-								</button>
-								<div class="dropdown-menu dropdown-menu-left" aria-labelledby="dropdownMenuDate" data-x-placement="bottom-end">
-									<a class="dropdown-item" href="#">2015</a>
-									<a class="dropdown-item" href="#">2016</a>
-									<a class="dropdown-item" href="#">2017</a>
-									<a class="dropdown-item" href="#">2018</a>
-								</div>
-							</div>
-						</div>
-					</div>
+					
 				</div>
 				<!-- breadcrumb -->
 @endsection
 @section('content')
 				<!-- row -->
 				<div class="row">
-
+					
+			
+						<!--/div-->
+	
+						<!--div-->
+					
+						<!--/div-->
+	
+						<!--div-->
+						<div class="col-xl-12">
+							<div class="card mg-b-20">
+								<div class="card-header pb-0">
+									<div class="d-flex justify-content-between">
+										<h4 class="card-title mg-b-0">Bordered Table</h4>
+										<i class="mdi mdi-dots-horizontal text-gray"></i>
+									</div>
+									<p class="tx-12 tx-gray-500 mb-2">Example of Valex Bordered Table.. <a href="">Learn more</a></p>
+								</div>
+								<div class="card-body">
+									<div class="table-responsive">
+										<table id="example1" class="table key-buttons text-md-nowrap">
+											<thead>
+												<tr>
+													<th class="border-bottom-0">#</th>
+													<th class="border-bottom-0">رقم الفاتوره</th>
+													<th class="border-bottom-0">تاريخ الفاتوره</th>
+													<th class="border-bottom-0">تاريخ الاستحقاق</th>
+													<th class="border-bottom-0">المنتج</th>
+													<th class="border-bottom-0">القسم</th>
+													<th class="border-bottom-0">الخصم</th>
+													<th class="border-bottom-0">نسبة الضريبه</th>
+													<th class="border-bottom-0">قيمة الضريبه</th>
+													<th class="border-bottom-0">الإجمالي</th>
+													<th class="border-bottom-0">الحاله</th>
+													<th class="border-bottom-0">ملاحظات</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>1</td>
+													<td>123555</td>
+													<td>2023-1-1</td>
+													<td>2023-1-1</td>
+													<td>cc</td>
+													<td>البنك الاهلي</td>
+													<td>1000</td>
+													<td>10%</td>
+													<td>$320,800</td>
+													<td>88895</td>
+													<td>غير مدفوعه</td>
+													<td>لم يتم السداد حتي الأن</td>
+												</tr>
+												
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!--/div-->
+	
+						<!--div-->
+						
+					
 				</div>
 				<!-- row closed -->
 			</div>
@@ -49,4 +98,23 @@
 		<!-- main-content closed -->
 @endsection
 @section('js')
+<!-- Internal Data tables -->
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+<!--Internal  Datatable js -->
+<script src="{{URL::asset('assets/js/table-data.js')}}"></script>
 @endsection
